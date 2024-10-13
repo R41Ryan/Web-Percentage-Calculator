f35704a12eb1793e4880a3caee860400b6e00c05
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2024-10-13 10:31:05 -0600

    added basic code

---
+++ script.js
@@ -0,0 +1,13 @@
+function calculatePercentage() 
+{
+    const inputNumber = document.getElementById("inputNumber").value;
+
+    const inputPercentage = document.getElementById("inputPercentage").value;
+    inputPercentage = inputPercentage / 100.0;
+
+    const result = inputNumber + inputNumber * inputPercentage;
+
+    document.getElementById("result").textContent = result;
+}
+
+document.getElementById("calculateNumber").addEventListener("click", calculatePercentage);
